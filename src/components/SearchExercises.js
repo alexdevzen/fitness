5e875470087de85d35d9102b4e9671273918a802
alexdevzen <alex.devzen@gmail.com>
2024-03-23 15:55:14 -0300

    Exercises category and details

--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -1,19 +1,43 @@
 import React, { useEffect, useState } from "react";
 import { Box, Button, Stack, TextField, Typography } from "@mui/material";
 
+// Import Components 
 import { exerciseOptions, fetchData } from "../utils/fetchData";
+import HorizontalScrollbar from "./HorizontalScrollbar";
 
-const SearchExercises = () => {
+const SearchExercises = ({ setExercises, bodyPart, setBodyPart }) => {
   const [search, setSearch] = useState("");
+  const [bodyParts, setBodyParts] = useState([]);
+
+  useEffect(() => {
+    const fetchExercisesData = async ( ) => {
+      const bodyPartsData = await fetchData(
+        "https://exercisedb.p.rapidapi.com/exercises/bodyPartList",
+        exerciseOptions
+      );
+
+      setBodyParts(["all", ...bodyPartsData]);
+    };
+
+    fetchExercisesData();
+  }, []);
 
   const handleSearch = async () => {
     if (search) {
       const exercisesData = await fetchData(
-        "https://exercisedb.p.rapidapi.com/exercises/bodyPart/back",
+        "https://exercisedb.p.rapidapi.com/exercises",
         exerciseOptions
       );
 
-      console.log(exercisesData);
+      const searchedExercises = exercisesData.filter(
+        (exercise) =>
+          exercise.name.toLowerCase().includes(search) ||
+          exercise.target.toLowerCase().includes(search) ||
+          exercise.equipment.toLowerCase().includes(search) ||
+          exercise.bodyPart.toLowerCase().includes(search)
+      );
+      setSearch("");
+      setExercises(searchedExercises);
     }
   };
 
@@ -68,6 +92,19 @@ const SearchExercises = () => {
           Buscar
         </Button>
       </Box>
+
+      <Box
+        sx={{
+          position: "relative",
+          width: "100%",
+          p: "20px",
+        }}
+      >
+        <HorizontalScrollbar data={bodyParts}
+        bodyPart={bodyPart} setBodyPart={setBodyPart}
+        />
+      </Box>
+    
     </Stack>
   );
 };
